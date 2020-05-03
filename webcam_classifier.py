39cec158dd353569879b45da7de37cb756524c77
Kris Zhao <kriszhao@comcast.net>
2020-05-02 19:58:03 -0700

    Cleaned up and commented webcam_classifier

--- webcam_classifier.py
+++ webcam_classifier.py
@@ -11,89 +11,91 @@ from PIL import Image
 import os
 import pathlib
 
-def process_frame(q, frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    int_img = np.cumsum(np.cumsum(gray, axis=0), axis=1)
-    # print(frame.shape)
-    # bar = progressbar.ProgressBar()
-    faces_found = 0
-    start = time.time()
-    for y in range(gray.shape[0]-model_shape[0]):
-        for x in range(gray.shape[1]-model_shape[1]):
-            pred = model.classify(int_img[y:y+model_shape[0], x:x+model_shape[1]], recalc=False)
-            # faces_found += pred
-            # print('Found {} faces'.format(faces_found))
-            if pred == 1:
-                # draw a window
-                cv2.rectangle(frame, (x, y), (x+model_shape[1], y+model_shape[0]), (0, 255, 0))
-            # pass
-    q.put(frame)
-
 if __name__ == '__main__':
+    # Define the model shape we trained with
     model_shape = (19, 19) # dataset size
-    model = AdaBoostModel.load('test_run_cascade')
-    cam = cv2.VideoCapture(0)
-    # cv2.namedWindow("test")
-    # q = Queue(-1)
-    img_counter = 0
+    
+    # Specify the model file to be used and camera number
+    model_file = 'test_run_cascade_600'
+    camera_number = 0
 
+    #Define both the model and the camera to capture and define from
+    model = AdaBoostModel.load(model_file)
+    cam = cv2.VideoCapture(camera_number)
+
+    # Loop through taking camera input
     while True:
+        #Read the frame and determine success of reading
         ret, frame = cam.read()
-        if frame is None:
-            continue
-        orig_shape = (frame.shape[1], frame.shape[0])
-        # frame = cv2.imread('classmates.png')
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # scale_percent = 25
-        # width = int(frame.shape[1] * scale_percent / 100)
-        # height = int(frame.shape[0] * scale_percent / 100) 
-        dim = (160, 120)
-        resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-        int_img = np.cumsum(np.cumsum(gray, axis=0), axis=1)
-        # # print(frame.shape)
-        # # bar = progressbar.ProgressBar()
-        faces_found = 0
-        start = time.time()
-        max_zoom = 2.0
-        step = 1.3
-        zoom = 1.0
-        tmp = resized
-        while zoom < max_zoom:
-            for y in range(tmp.shape[0]-model_shape[0]):
-                for x in range(tmp.shape[1]-model_shape[1]):
-                    pred = model.classify(int_img[y:y+model_shape[0], x:x+model_shape[1]], recalc=False)
-                    # faces_found += pred
-                    # print('Found {} faces'.format(faces_found))
-                    if pred == 1:
-                        # draw a window
-                        cv2.rectangle(resized, (int(x*zoom), int(y*zoom)), (int((x+model_shape[1])*zoom), int((y+model_shape[0])*zoom)), (0, 255, 0))
-                    # pass
-            zoom *= step
-            tmp = cv2.resize(tmp, (int(tmp.shape[1]/zoom), int(tmp.shape[0]/zoom)), interpolation=cv2.INTER_AREA)
 
-        stop = time.time()
-        print(stop - start)
-        # def classify_and_draw(gray, frame, y, x):
-        #     pred = model.classify(gray[y:y+model_shape[1], x:x+model_shape[0]])
-        #         # faces_found += pred
-        #         # print('Found {} faces'.format(faces_found))
-        #     if pred == 1:
-        #         # draw a window
-        #         cv2.rectangle(frame, (x, y), (x+model_shape[0], y+model_shape[0]), (0, 255, 0))
-        
-        # Pool(4).apply(classify_and_draw, np.ndenumerate(gray), {'gray': gray, 'frame': frame})
-        # # # cv2.putText(frame, "Found {} faces".format(int(faces_found)), (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        new_frame = cv2.resize(resized, orig_shape, interpolation=cv2.INTER_AREA)
-        cv2.imshow("Faces", new_frame)
-        # cv2.waitKey()
+        # If there is a camera error, we break
         if not ret:
             break
+
+        if frame is None:
+            continue
+        
+        # Determine the image shape python is reading in from the webcam
+        orig_shape = (frame.shape[1], frame.shape[0])
+        
+        # New dimensions for the image (width, height)
+        dim = (160, 120)
+        
+        # Lower the size for the image (to increase computations)
+        resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        
+        # Create a grayscale image (RGB colors -> GRAY)
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        
+        # Calculate our integral image:
+        int_img = calc_int(gray)
+
+        # Initiate a timer point to calculate time between images
+        start = time.time()
+        
+        # Run through our classification:
+        # Define the range of feature size regions (zoom = multiplied factor with feature of size 19 pixels)
+        zoom = 1.0
+        max_zoom = 2.0
+        step = 1.3
+
+        # Temporary resized image:
+        tmp = resized
+        
+        while zoom <= max_zoom:
+            #Loop through each possible feature location starting from top left:
+            for y in range(tmp.shape[0]-model_shape[0]):
+                for x in range(tmp.shape[1]-model_shape[1]):
+                    # Get our prediction for the model based on the target region
+                    pred = model.classify(int_img[y:y+model_shape[0], x:x+model_shape[1]], recalc=False)
+                    if pred == 1:
+                        # If we predict a feature, draw a box around the region (RGB)
+                        color = (0, 255, 0)
+
+                        cv2.rectangle(resized, (int(x*zoom), int(y*zoom)), (int((x+model_shape[1])*zoom), int((y+model_shape[0])*zoom)), color)
+                    # Else, do nothing   
+            zoom *= step
+            tmp = cv2.resize(tmp, (int(tmp.shape[1]/zoom), int(tmp.shape[0]/zoom)), interpolation=cv2.INTER_AREA)
+        
+        # End the computation time for each detection
+        stop = time.time()
+        print(stop - start)
+
+        # Resize the image back up to the original size
+        new_frame = cv2.resize(resized, orig_shape, interpolation=cv2.INTER_AREA)
+        
+        # Display the image
+        cv2.imshow("Faces", new_frame)
+
+        # Wait for a keypress and determine the key
         k = cv2.waitKey(1)
+
+        # If the key pressed during the 1 millesecond is the escape key:
         if k%256 == 27:
-            # ESC pressed
             print("Escape hit, closing...")
             break
+
+        # If we want to save, we can capture the previous image
         if k%256 == ord('s'):
             dirpath = os.path.abspath('./saves')
             filename = 'capture_at_{}.jpg'.format(time.strftime("%Y-%m-%d-%H-%M-%S"))
@@ -101,5 +103,7 @@ if __name__ == '__main__':
             path = pathlib.Path(filepath)
             Image.fromarray(new_frame.astype(np.uint8)).save(path, format="JPEG")
             print(f"Saved at {filename}")
+
+    # Cleanup resources:
     cam.release()
     cv2.destroyAllWindows()
